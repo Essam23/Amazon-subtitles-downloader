ba40e9a2a060c54591de1ccfb09614bf06e726ab
Essam3152 <87686356+Essam3152@users.noreply.github.com>
2021-11-23 05:07:18 +0200

    Update download.audio.bat

--- download.audio.bat
+++ download.audio.bat
@@ -1,3 +1,3 @@
 @echo off
-amazon.py -r pv -p pv -a amzn1.dv.gti.90b82cf5-c187-8341-ab5e-f1eacddaf9da --nosubs --novideo --keep --alang en --noAD
+amazon.py -r us -p us -a B08BYZKJSH --nosubs --novideo --keep --alang en --noAD
 pause
