ce60cf4052072d9fb44b52784638808058cae32b
Essam3152 <87686356+Essam3152@users.noreply.github.com>
2021-11-23 05:06:43 +0200

    Update download.video.bat

--- download.video.bat
+++ download.video.bat
@@ -1,3 +1,3 @@
 @echo off
-amazon.py -r pv -p pv -a amzn1.dv.gti.90b82cf5-c187-8341-ab5e-f1eacddaf9da --alang en --noAD --slang en -q 1080
+amazon.py -r us -p us -a B08BYZKJSH --alang en --noAD --slang en -q 1080
 pause
